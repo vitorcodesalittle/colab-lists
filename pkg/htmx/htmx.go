6a8ce3aae0a1b78cc2365a511f9ef590bccd25a5
vilma <vlma@cin.ufpe.br>
2024-09-15 01:24:03 -0300

    fixup

---
+++ pkg/htmx/htmx.go
@@ -0,0 +1,19 @@
+package htmx
+
+type HtmxMessage struct {
+	CurrentUrl  *string `json:"HX-Current-URL"`
+	Request     *string `json:"HX-Request"`
+	Target      *string `json:"HX-Target"`
+	Trigger     *string `json:"HX-Trigger"`
+	TriggerName *string `json:"HX-TriggerName"`
+	ActionType  *int    `json:"action-type"`
+}
+
+type HtmxMessageI struct {
+	Header HtmxMessage `json:"HEADERS"`
+	Any    interface{}
+}
+
+func (h *HtmxMessage) String() string {
+	return fmt.Sprintf("CurrentUrl: %s, Request: %s, Target: %s, Trigger: %s, TriggerName: %s, ActionType: %d\n", h.CurrentUrl, h.Request, h.Target, h.Trigger, h.TriggerName, h.ActionType)
+}
